dfdb9664a561cf23c07233ba78e1240b08c218f7
eshandro <erik843@gmail.com>
2025-01-20 14:41:22 -0700

    Adds shadcn and updates pnpm and sets a node version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import {Button} from '@/components/ui/button';
+
 export default function Home() {
   return (
     <section className="py-12">
       <div className="container">
         <h1 className="font-sans text-3xl font-bold">PORTFOLIO</h1>
+        <Button>Here is a button</Button>
       </div>
     </section>
   );
